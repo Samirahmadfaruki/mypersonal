435be721aa00b034cb4ab14ff9cd4e2f4037bdee
samirahmadfaruki <samirahmedfaruki@gmail.com>
2025-04-04 12:14:30 +0530

    dfgf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'Samirahmadfaruki/mypersonal/', // ✅ use your GitHub repo name here
+  base: '/', // ✅ use your GitHub repo name here
   plugins: [react()],
 })
